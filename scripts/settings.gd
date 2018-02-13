ca463ef74e5fe28881d9cf4584d949fc7a4e2cc8
Samuel <samuel.p991@gmail.com>
2018-02-13 19:40:07 +0100

    settings window animation

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -1,15 +1,27 @@
 extends Node2D
 
+var animation
+var is_closing = false
+
 func setup(music_on, sound_on, pos):
 	var music_switch = get_node("window/music_control/switch")
 	var sound_switch = get_node("window/sound_control/switch")
 	music_switch.set_pressed(not music_on)
 	sound_switch.set_pressed(not sound_on)
 	set_pos(pos)
+	animation = get_node("animation")
 
 func close():
-	get_tree().set_pause(false)
-	queue_free()
+	animation.play("close")
+	is_closing = true
 
 func _on_empty_space_pressed():
 	close()
+
+func _on_settings_enter_tree():
+	animation.play("open")
+
+func _on_animation_finished():
+	if is_closing:
+		get_tree().set_pause(false)
+		queue_free()
