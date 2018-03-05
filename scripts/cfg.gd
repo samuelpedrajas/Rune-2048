16ee5c4ad4d4f8899415f7a197a8c3b1d60ef6f6
Samuel <samuel.p991@gmail.com>
2018-03-05 15:17:36 +0100

    papire

--- scripts/cfg.gd
+++ scripts/cfg.gd
@@ -16,7 +16,7 @@ const MOTION_DISTANCE = 15  # Minimum distance with the mouse pressed to make a 
 const MINIMUM_DISTANCE_TO_MOVE = 0.6 # Minimum distance from the direction vectors to make a move
 
 const SETTINGS_WINDOW_POS = Vector2(540, 1000)
-const BOOK_WINDOW_POS = Vector2(540, 1000)
+const BOOK_WINDOW_POS = Vector2(540, 850)
 const EXIT_WINDOW_POS = Vector2(540, 850)
 const RESET_WINDOW_POS = Vector2(540, 850)
 const SCROLL_THRESHOLD = 10
